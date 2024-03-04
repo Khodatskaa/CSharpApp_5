45a335545b39c572a0d60c7b17d5f26b27280324
Khodatskaa <khodatskaalina@gmail.com>
2024-03-04 11:22:30 +0200

    Ex 1 was done

--- CSharpApp_5/Program.cs
+++ CSharpApp_5/Program.cs
@@ -2,8 +2,16 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int[] testArray = { 2, 5, 8, 12, 15, 18, 22, 25 };
+            ArrayCalculator arrayCalculator = new ArrayCalculator(testArray);
+
+            int lessCount = arrayCalculator.Less(20);
+            int greaterCount = arrayCalculator.Greater(10);
+
+            Console.WriteLine("Number of elements less than 20: " + lessCount);
+            Console.WriteLine("Number of elements greater than 10: " + greaterCount);
         }
     }
 }
--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -1,11 +1,36 @@
+using CSharpApp_5;
+
 namespace TestProject
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Less_ShouldReturnCorrectCount()
         {
+            // Arrange
+            int[] testArray = { 2, 5, 8, 12, 15, 18, 22, 25 };
+            ArrayCalculator arrayCalculator = new ArrayCalculator(testArray);
+
+            // Act
+            int lessCount = arrayCalculator.Less(20);
+
+            // Assert
+            Assert.AreEqual(5, lessCount);
+        }
+
+        [TestMethod]
+        public void Greater_ShouldReturnCorrectCount()
+        {
+            // Arrange
+            int[] testArray = { 2, 5, 8, 12, 15, 18, 22, 25 };
+            ArrayCalculator arrayCalculator = new ArrayCalculator(testArray);
+
+            // Act
+            int greaterCount = arrayCalculator.Greater(10);
+
+            // Assert
+            Assert.AreEqual(7, greaterCount);
         }
     }
 }
